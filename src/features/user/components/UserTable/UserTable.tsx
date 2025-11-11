5d664b4bcade4fb7a33362d1cca7b61711e04d78
sergeidobri <teplichkinteplichkinov@gmail.com>
2025-11-11 11:22:12 +0300

    Optimized table JSX code in UserTable

--- src/features/user/components/UserTable/UserTable.tsx
+++ src/features/user/components/UserTable/UserTable.tsx
@@ -7,54 +7,26 @@ type User = {
   lastName: string;
   affiliation?: string | null;
   country?: string | null; // в будущем - что-то одно из выпадающего списка стран
-  ORCID?: string | null;
-  webPage?: string | null;
 };
 
 const UserTable = ({ user }: { user: User }) => {
+  const infoToDisplay = [
+    { label: 'Email', value: user.email },
+    { label: 'Title', value: user.title },
+    { label: 'First Name', value: user.firstName },
+    { label: 'Last Name', value: user.lastName },
+    { label: 'Affiliation', value: user.affiliation },
+    { label: 'Country', value: user.country },
+  ];
   return (
     <table className={styles.table}>
       <tbody>
-        <tr>
-          <th>Email</th>
-          <td>{user.email}</td>
-        </tr>
-        <tr>
-          <th>Title</th>
-          <td>{user.title || ' '}</td>
-        </tr>
-        <tr>
-          <th>First Name</th>
-          <td>{user.firstName}</td>
-        </tr>
-        <tr>
-          <th>Last Name</th>
-          <td>{user.lastName}</td>
-        </tr>
-        <tr>
-          <th>Affiliation</th>
-          <td>{user.affiliation || ' '}</td>
-        </tr>
-        <tr>
-          <th>Country</th>
-          <td>{user.country || ' '}</td>
-        </tr>
-        <tr>
-          <th>ORCID</th>
-          <td>{user.ORCID || ' '}</td>
-        </tr>
-        <tr>
-          <th>Web Page</th>
-          <td>
-            {user.webPage ? (
-              <a href={user.webPage} target="_blank" rel="noreferrer">
-                {user.webPage}
-              </a>
-            ) : (
-              ' '
-            )}
-          </td>
-        </tr>
+        {infoToDisplay.map(item => (
+          <tr>
+            <th>{item.label}</th>
+            <td>{item.value || ' '}</td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
